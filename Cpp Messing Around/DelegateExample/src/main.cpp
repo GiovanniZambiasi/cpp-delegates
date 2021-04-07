df4ef49c821a270ca32d4540a54e32f58138082c
Giovanni Zambiasi <giovanni.zambiasi@gmail.com>
2021-04-07 20:33:26 -0300

    Add support for any number of parameters. Separate delegate into .dll

---
+++ Cpp Messing Around/DelegateExample/src/main.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <vector>
+
+#include "Delegate.h"
+
+using namespace std;
+
+class Foo
+{
+public:
+
+    void DoFoo()
+    {
+        cout << "Foo!" << endl;
+    }
+
+    void DoBar()
+    {
+        cout << "Bar!" << endl;
+    }
+    
+    void DoFooWithParams(float a, int b, bool c)
+    {
+        cout << "Foo with params! " << a << " " << b << " " << c << endl;
+    }
+};
+
+
+void main()
+{
+    Delegate simpleDelegate;
+    
+    Foo* foo = new Foo();
+    simpleDelegate.Add(foo, &Foo::DoFoo);
+    
+    Foo* fooOther = new Foo();
+    simpleDelegate.Add(fooOther, &Foo::DoBar);
+
+    simpleDelegate.Invoke();
+
+    Delegate<float, int, bool> specialized;
+
+    specialized.Add(foo, &Foo::DoFooWithParams);
+
+    specialized.Invoke(10.0f, 1, false);
+    
+    system("pause");
+}
